34cb5f527636c88b7b3754f72742f49c1788c601
metus01 <carnelmetus@gmail.com>
2026-03-12 01:04:30 +0100

    étude du sujet

---
+++ maxpar.py
@@ -0,0 +1,5 @@
+class Task:
+    name ="" #nom de la tâche
+    reads=[] # domaine de lecture de la tâche
+    writes=[] # domaine d'écriture de la tâche
+    run=None # la fonction qui déterminera le comportement de la tâche

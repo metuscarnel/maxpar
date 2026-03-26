838bbf2fd0de3546a573157158c588607daa5527
metus01 <carnelmetus@gmail.com>
2026-03-26 10:39:53 +0100

    update the files

--- maxpar.py
+++ maxpar.py
@@ -5,12 +5,14 @@ import threading
 import random
 from collections import defaultdict
 
+
 class Task:
     def __init__(self, name, reads, writes, run):
         self.name = name
         self.reads = reads
         self.writes = writes
         self.run = run
+
 
 class TaskSystem:
     def __init__(self, tasks=None, precedences_map=None):
@@ -19,6 +21,7 @@ class TaskSystem:
         for task in self.tasks:
             if task.name not in self.precedences_map:
                 self.precedences_map[task.name] = []
+
     def check_input(self):
         if len(self.tasks) == 0:
             raise ValueError("Aucune tâche n'est définie dans le système.")
@@ -30,12 +33,16 @@ class TaskSystem:
         for task, dependencies in self.precedences_map.items():
             task_obj = next((t for t in self.tasks if t.name == task), None)
             if task_obj is None:
-                raise ValueError(f"Tache {task} dans la map de précédences n'existe pas.")
+                raise ValueError(
+                    f"Tache {task} dans la map de précédences n'existe pas."
+                )
             for dep in dependencies:
                 dep_task = next((t for t in self.tasks if t.name == dep), None)
                 if dep_task is None:
-                    raise ValueError(f"Tache {dep} dans la map de précédences n'est pas dans la liste des tâches.")
-        
+                    raise ValueError(
+                        f"Tache {dep} dans la map de précédences n'est pas dans la liste des tâches."
+                    )
+
     def getDependancies(self, task_name):
         dependancies = []
         for task, dependencies in self.precedences_map.items():
@@ -120,20 +127,26 @@ class TaskSystem:
     def is_determinated_system(self):
         dir_graph = self._build_graph()
         for task1, task2 in itertools.combinations(self.tasks, 2):
-            if not self.has_path(dir_graph, task1.name, task2.name) and not self.has_path(dir_graph, task2.name, task1.name):
-                
+            if not self.has_path(
+                dir_graph, task1.name, task2.name
+            ) and not self.has_path(dir_graph, task2.name, task1.name):
+
                 if not self.bernstein_conditions(task1, task2):
-                    print(f"{task1.name} et {task2.name} sont parallèles mais ne respectent pas les conditions de Bernstein. Système non déterminé.")
+                    print(
+                        f"{task1.name} et {task2.name} sont parallèles mais ne respectent pas les conditions de Bernstein. Système non déterminé."
+                    )
                     return False
         return True
 
     def generate_system_max(self):
         if not self.is_determinated_system():
-            raise ValueError("Le système n'est pas déterminé. Impossible de construire le système de parallélisme maximal.")
-            
+            raise ValueError(
+                "Le système n'est pas déterminé. Impossible de construire le système de parallélisme maximal."
+            )
+
         dir_graph = self._build_graph()
         smax_precedences = {task.name: [] for task in self.tasks}
-        
+
         for task1, task2 in itertools.permutations(self.tasks, 2):
             if self.has_path(dir_graph, task1.name, task2.name):
                 if not self.bernstein_conditions(task1, task2):
@@ -151,11 +164,11 @@ class TaskSystem:
         dot = graphviz.Digraph(comment="Graphe du système de parallélisme maximal")
         for task in self.tasks:
             dot.node(task.name)
-            
+
         for task, dependencies in self.precedences_map.items():
             for dep in dependencies:
                 dot.edge(dep, task)
-                
+
         dot.render(filename, format="png", cleanup=True)
 
     def detTestRnd(self, dict_globals, nb_iterations=3):
@@ -166,27 +179,25 @@ class TaskSystem:
 
         for i in range(nb_iterations):
             inits_values = {var: random.randint(1, 100) for var in all_variables}
-            
-            
+
             for var, value in inits_values.items():
                 dict_globals[var] = value
             self.run()
             result1 = {var: dict_globals[var] for var in all_variables}
-            
-            
+
             for var, value in inits_values.items():
                 dict_globals[var] = value
             self.run()
             result2 = {var: dict_globals[var] for var in all_variables}
-            
+
             if result1 != result2:
                 print("Le système n'est pas déterministe (Test Randomisé Échoué).")
                 return False
-                
+
         print("Le système est déterministe (Test Randomisé Validé).")
         return True
 
-    def parCost(self, nb_iterations=4):  
+    def parCost(self, nb_iterations=4):
         seq_time = 0
         for _ in range(nb_iterations):
             start = time.perf_counter()
@@ -198,14 +209,12 @@ class TaskSystem:
         for _ in range(nb_iterations):
             start = time.perf_counter()
             self.run()
-            par_time += time.perf_counter() - start            
+            par_time += time.perf_counter() - start
         moyenne_par = par_time / nb_iterations
 
         print(f"Moyenne exécution séquentielle : {moyenne_seq:.4f} secondes")
         print(f"Moyenne exécution parallèle    : {moyenne_par:.4f} secondes")
 
-
     def draw_all(self):
         self.draw("temp_graph")
         self.generate_system_max().draw("temp_graph_smax")
-    
--- test.py
+++ test.py
@@ -1,33 +1,46 @@
 import time
 from maxpar import Task, TaskSystem
 
+# Nous testons ici le système de tâche au point 4.4 du fichier des notes de cours disponible sur Ecampus
 memoire = {"M1": 0, "M2": 0, "M3": 0, "M4": 0, "M5": 0}
+
 
 def run_t1():
     time.sleep(0.5)
-    memoire["M4"] = memoire["M1"] +3
+    memoire["M4"] = memoire["M1"] + 3
     print("[T1] écrit dans M4 et lit dans M1")
+
+
 def run_t2():
     time.sleep(0.5)
     memoire["M1"] = memoire["M3"] + memoire["M4"] + 2
     print("[T2] écrit dans M1 et lit dans M3, M4")
+
+
 def run_t3():
     time.sleep(0.5)
     memoire["M5"] = memoire["M3"] + memoire["M4"] + 1
     print("[T3] écrit dans M5 et lit dans M3, M4")
+
+
 def run_t4():
     time.sleep(0.5)
     memoire["M2"] = memoire["M4"] + 4
     print("[T4] écrit dans M2 et lit dans M4")
+
+
 def run_t5():
     time.sleep(0.5)
     memoire["M5"] = memoire["M5"] + 6
     print("[T5] écrit dans M5 et lit dans M5")
+
+
 def run_t6():
     time.sleep(0.5)
     memoire["M4"] = memoire["M1"] + memoire["M2"] + 3
     print("[T6] écrit dans M4 et lit dans M1, M2")
-  
+
+
 if __name__ == "__main__":
 
     t1 = Task("T1", reads=["M1"], writes=["M4"], run=run_t1)
@@ -38,17 +51,17 @@ if __name__ == "__main__":
     t6 = Task("T6", reads=["M1", "M2"], writes=["M4"], run=run_t6)
 
     tasks = [t1, t2, t3, t4, t5, t6]
-    
+
     precedence = {
-        "T1": [],            
-        "T2": ["T1"],       
-        "T3": ["T1"],        
-        "T4": ["T2"],        
-        "T5": ["T2", "T3"],  
-        "T6": ["T4", "T5"]  
+        "T1": [],
+        "T2": ["T1"],
+        "T3": ["T1"],
+        "T4": ["T2"],
+        "T5": ["T2", "T3"],
+        "T6": ["T4", "T5"],
     }
 
     sys = TaskSystem(tasks=tasks, precedences_map=precedence)
     sys.check_input()
-    sys.parCost()  
-    sys.draw_all() 
+    sys.parCost()
+    sys.draw_all()
